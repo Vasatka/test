52db186886fd9ff823caabe45b7d8ec221ec21ca
Vasatka <143732538+Vasatka@users.noreply.github.com>
2023-08-31 18:20:57 +0300

    Add files via upload

---
+++ Project/Project/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Col_num1 = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Col_var = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Col_num2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Col_res = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Col_num1,
+            this.Col_var,
+            this.Col_num2,
+            this.Col_res});
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 50;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 57);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Col_num1
+            // 
+            this.Col_num1.HeaderText = "Number1";
+            this.Col_num1.MinimumWidth = 6;
+            this.Col_num1.Name = "Col_num1";
+            this.Col_num1.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Col_num1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // Col_var
+            // 
+            this.Col_var.HeaderText = "Actions";
+            this.Col_var.MinimumWidth = 6;
+            this.Col_var.Name = "Col_var";
+            // 
+            // Col_num2
+            // 
+            this.Col_num2.HeaderText = "Number2";
+            this.Col_num2.MinimumWidth = 6;
+            this.Col_num2.Name = "Col_num2";
+            // 
+            // Col_res
+            // 
+            this.Col_res.HeaderText = "Result";
+            this.Col_res.MinimumWidth = 6;
+            this.Col_res.Name = "Col_res";
+            this.Col_res.ReadOnly = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(368, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Calcute";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 154);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Col_num1;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Col_var;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Col_num2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Col_res;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Project/Project/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            dataGridView1.Rows.Add();
+            Col_num1.DataSource = new List<string>() { "6", "77", "12345", "8218", "0", "5", "2", "8" };
+            Col_var.DataSource = new List<string>() { "*", "/", "-" };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows[0].Cells[0].Value != null &&
+                dataGridView1.Rows[0].Cells[1].Value != null &&
+                dataGridView1.Rows[0].Cells[2].Value != null)
+            {
+                double n1 = Convert.ToDouble(dataGridView1.Rows[0].Cells[0].Value);
+                string token = dataGridView1.Rows[0].Cells[1].Value.ToString();
+                double result = 0;
+                try
+                {
+                    double n2 = Convert.ToDouble(dataGridView1.Rows[0].Cells[2].Value);
+                    switch (token)
+                    {
+                        case "*":
+                            result = n1 * n2;
+                            break;
+                        case "/":
+                            result = n1 / n2;
+                            break;
+                        case "-":
+                            result = n1 - n2;
+                            break;
+                    }
+
+                    dataGridView1.Rows[0].Cells[3].Value = result.ToString();
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show("Неверный формат входных данных");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Заполните все поля");
+            }
+        }
+    }
+}
